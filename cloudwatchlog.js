37ad1802b09f78c0d422ad4cc6444add099b85b4
Srimanta Jana <srimantajana@live.com>
2024-10-13 15:47:36 -0700

    initial commit

---
+++ cloudwatchlog.js
@@ -0,0 +1,55 @@
+const AWS = require("aws-sdk");
+const fs = require("fs");
+const path = require("path");
+
+const region = process.env.awsregion;
+const logGroupName = `/ecs/${process.env.CONTAINER_NAME}`;
+const logStreamName = `ecs/${process.env.CONTAINER_NAME}`; // Ensure this matches your log configuration
+const bucketName = process.env.bucket;
+const reportPath = process.env.reportpath;
+const containerName = process.env.CONTAINER_NAME;
+const suiteId = process.env.suiteid;
+
+AWS.config.update({ region: region });
+
+const cloudWatchLogs = new AWS.CloudWatchLogs();
+
+async function getLogEvents() {
+  try {
+    const params = {
+      logGroupName,
+      logStreamName,
+      startFromHead: true,
+    };
+
+    const data = await cloudWatchLogs.getLogEvents(params).promise();
+    console.log("Log events:", data.events);
+
+    const logData = data.events.map((event) => event.message).join("\n");
+    const logFilePath = path.join(reportPath, "cloudwatch-logs.txt"); // Adjust the path as needed
+
+    fs.writeFileSync(logFilePath, logData, "utf8");
+    console.log("Logs saved to:", logFilePath);
+
+    // Upload the logs to S3
+    const s3 = new AWS.S3();
+    const keyName = `logs/${containerName}_${suiteId}.log`;
+
+    await s3
+      .putObject({
+        Bucket: bucketName,
+        Key: keyName,
+        Body: logData,
+      })
+      .promise();
+
+    console.log("Log file successfully uploaded to S3:", keyName);
+
+    const s3Url = `https://${bucketName}.s3.${region}.amazonaws.com/${keyName}`;
+    console.log("Access the log file at:", s3Url);
+  } catch (error) {
+    console.error("Error fetching logs:", error);
+  }
+}
+
+getLogEvents();
