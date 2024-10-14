59bcb119c5c96ba86e1742e07375b0554e7ac251
Srimanta Jana <srimantajana@live.com>
2024-10-14 08:54:25 -0700

    updated file

--- cloudwatchlog.js
+++ cloudwatchlog.js
@@ -2,19 +2,27 @@ const AWS = require("aws-sdk");
 const fs = require("fs");
 const path = require("path");
 
+// Credentials (ensure these are securely managed in production)
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
 const region = process.env.awsregion;
 const bucketName = process.env.bucket;
 const reportPath = process.env.reportpath;
 const containerName = process.env.containername;
 const suiteId = process.env.suiteid;
 
-AWS.config.update({ region: region });
+AWS.config.update({
+  region: region,
+  accessKeyId: accessKeyId,
+  secretAccessKey: secretAccessKey,
+});
 
 const readAndUploadLog = async () => {
   try {
     const logFilePath = path.join("/repo", "test.log"); // Path to the log file
     const logData = fs.readFileSync(logFilePath, "utf8"); // Read the log file
-    console.log("logData=" + logData);
+
     const s3 = new AWS.S3();
     const keyName = `logs/${containerName}_${suiteId}.log`;
 
