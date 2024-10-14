4cd4e80b88e122b83a296d0654cb8555384a42fc
Srimanta Jana <srimantajana@live.com>
2024-10-13 21:03:43 -0700

    updated file

--- cloudwatchlog.js
+++ cloudwatchlog.js
@@ -3,12 +3,16 @@ const fs = require("fs");
 const path = require("path");
 
 const region = process.env.awsregion;
-const logGroupName = `/ecs/${process.env.CONTAINER_NAME}-${objid}`;
+const logGroupName = `/ecs/${process.env.CONTAINER_NAME}-${process.env.suiteid}`;
 const logStreamName = `ecs/${process.env.CONTAINER_NAME}`; // Ensure this matches your log configuration
 const bucketName = process.env.bucket;
 const reportPath = process.env.reportpath;
 const containerName = process.env.CONTAINER_NAME;
 const suiteId = process.env.suiteid;
+
+console.log("suiteId=" + suiteId);
+
+console.log("bucketName=" + bucketName);
 
 AWS.config.update({ region: region });
 
